0aed268b19d1116e147635444efaddac8ae2d0f6
Dmitry Jerusalimsky <dmitry_jerusalimsky@cable.comcast.com>
2014-11-13 09:58:56 -0800

    Minor updates. included sorting

--- Scripts/ACE_Commute.js
+++ Scripts/ACE_Commute.js
@@ -1,16 +1,21 @@
+// get JSON from %ace_res variable in Tasker
+var jsonStr = local('ace_res');
+
+var json = JSON.parse(jsonStr);
+var data = json.get_vehicles;
+
 // update last updated time
 var gen_on = new Date();
 setLocal('%ace_last_update', gen_on.getHours() + ":" + gen_on.getMinutes());
-
-// get JSON from %rtres variable in Tasker
-var json = JSON.parse(local('rtres'));
-var data = json.get_vehicles;
 
 // define list of stations of my interest
 var fremont = 156;
 var great_america = 157;
 
+var fremontAmArr = [], fremontPmArr = [], gaAmArr = [], gaPmArr = [];
+
 for (var i = 0; i < data.length; i++) {
+  // if next stop is not specified, skip it
   if (data[i].nextStopID == null) {
     continue;
   }
@@ -24,23 +29,57 @@ for (var i = 0; i < data.length; i++) {
     // get mins remaining
     var minsLeft = curStop.minutes;
     var arrTime = curStop.time;
-    var append = "";
-    if (arrTime.toLowerCase().indexOf("am")) {
-      append = "_am";
-    }
-    else {
-      append = "_pm";
-    }
+    var isAm = arrTime.toLowerCase().indexOf("am");
 
+    // add values to the appropriate arrays
     if (stopID == fremont) {
-      setLocal("ace_fremont" + append, minsLeft);
+      if (isAm) {
+        fremontAmArr.push(minsLeft);
+      }
+      else {
+        fremontPmArr.push(minsLeft);
+      }
     }
 
     else if (stopID == great_america) {
-      setLocal("ace_ga" + append, minsLeft);
+      if (isAm) {
+        gaAmArr.push(minsLeft);
+      }
+      else {
+        gaPmArr.push(minsLeft);
+      }
     }
     else {
       continue;
     }
   }
 }
+
+// define a sort function to sort in ascending order
+var sortFunc = function(a, b) {
+  return a - b;
+};
+
+// sort arrays
+gaAmArr.sort(sortFunc);
+gaPmArr.sort(sortFunc);
+fremontAmArr.sort(sortFunc);
+fremontPmArr.sort(sortFunc);
+
+// join first 2 elements of arrays
+var fAm = fremontAmArr.slice(0, 2).join(" & ");
+var fPm = fremontPmArr.slice(0, 2).join(" & ");
+var gaAm = gaAmArr.slice(0, 2).join(" & ");
+var gaPm = gaPmArr.slice(0, 2).join(" & ");
+
+// if results are empty, set them to Not In Service
+if (fAm == "") { fAm = "NIS"; }
+if (fPm == "") { fPm = "NIS"; }
+if (gaAm == "") { gaAm = "NIS"; }
+if (gaPm == "") { gaPm = "NIS"; }
+
+// set local variables
+setLocal("ace_fremont_am", fAm);
+setLocal("ace_fremont_pm", fPm);
+setLocal("ace_ga_am", gaAm);
+setLocal("ace_ga_pm", gaPm);
--- Scripts/VTA_Commute.js
+++ Scripts/VTA_Commute.js
@@ -1,5 +1,5 @@
 // get JSON from a Tasker Variable
-var json = JSON.parse(local('rtres'));
+var json = JSON.parse(local('vta_res'));
 var data = json.data;
 
 // list of stations of interest
@@ -12,6 +12,8 @@ var mp_home_id = 8040508;
 var now_date = new Date();
 var now_date_ms = Date.parse(now_date);
 
+var lmWorkArr = [], lmHomeArr = [], mpWorkArr = [], mpHomeArr = [];
+
 // get time when response was generated at
 var gen_on_ms = Date.parse(json.generated_on);
 var gen_on = new Date(new Date().setTime(gen_on_ms));
@@ -21,29 +23,52 @@ setLocal('%vta_last_update', gen_on.getHours() + ":" + gen_on.getMinutes());
 for (var i = 0; i < data.length; i++) {
     var arrivals = data[i].arrivals;
     var stop_id = data[i].stop_id;
-    var cur_arr_count = 0;
-    var cur_times = "";
 
     // iterate over arrivals for a given stop
     for (var j = 0; j < arrivals.length; j++) {
-        if (cur_arr_count > 1) { break; }
         var arr_date = Date.parse(arrivals[j].arrival_at);
         var min_diff = Math.floor((arr_date - now_date_ms) / 1000 / 60);
-        cur_times += min_diff + " & ";
-        cur_arr_count++;
-    }
-    cur_times = cur_times.substring(0, cur_times.length - 3);
 
-    if (stop_id == lm_work_id) {
-      setLocal('%lmwork', cur_times);
-    }
-    else if (stop_id == lm_home_id) {
-      setLocal('%lmhome', cur_times);
-    }
-    else if (stop_id == mp_work_id) {
-      setLocal('%mpwork', cur_times);
-    }
-    else if (stop_id == mp_home_id) {
-      setLocal('%mphome', cur_times);
+        // push current times to an appropriate array
+        if (stop_id == lm_work_id) {
+          lmWorkArr.push(min_diff);
+        }
+        else if (stop_id == lm_home_id) {
+          lmHomeArr.push(min_diff);
+        }
+        else if (stop_id == mp_work_id) {
+          mpWorkArr.push(min_diff);
+        }
+        else if (stop_id == mp_home_id) {
+          mpHomeArr.push(min_diff);
+        }
     }
 }
+
+// define a sort function (ascending order)
+var sortFunc = function(a, b) {
+  return a - b;
+}
+
+mpWorkArr.sort(sortFunc);
+mpHomeArr.sort(sortFunc);
+lmWorkArr.sort(sortFunc);
+lmHomeArr.sort(sortFunc);
+
+// join first 2 elements of arrays
+var lmwork = lmWorkArr.slice(0, 2).join(" & ");
+var lmhome = lmHomeArr.slice(0, 2).join(" & ");
+var mpwork = mpWorkArr.slice(0, 2).join(" & ");
+var mphome = mpHomeArr.slice(0, 2).join(" & ");
+
+// if results are empty, set them to Not In Service
+if (lmwork == "") { lmwork = "NIS"; }
+if (lmhome == "") { lmhome = "NIS"; }
+if (mpwork == "") { mpwork = "NIS"; }
+if (mphome == "") { mphome = "NIS"; }
+
+// set local variables
+setLocal('lmwork', lmwork);
+setLocal('lmhome', lmhome);
+setLocal('mpwork', mpwork);
+setLocal('mphome', mphome);
